c4750e9d71cfc1053325175ea42bf3dec341f97d
向雪莲 <2424599257@qq.com>
2018-08-25 12:09:37 +0800

    docs: again

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -12,7 +12,7 @@ router.post("/", async (req, res, next) => {
   logger.info("want to create the page: ", pageName)
 
   const rootPath = './projects/'+ projectName +'/src/views/'
-  const _page = '<!--页面名称：'+ pageName +' 页面路径 '+ rootPath +'-->\r<template>\n  <div class="'+ pageName +'-page"></div>\n<template>\n<script type="text/javascript">\n  export default {\n    data() {\n      return{\n\n      }  \n    },\n    computed: {\n\n    },\n    beforeCreate() {\n\n    },\n    created() {\n\n    },\n    beforeMount() {\n\n    },\n    mounted() {\n\n    }, \n    beforeUpdate() {\n\n    }, \n    updated() {\n\n    }, \n    beforeDestroy() {\n\n    },\n    destroyed() {\n\n    }, \n    methods: {\n\n    },\n    watch: {\n\n    }\n  };\n</script>\n<style scoped> \n\n</style>'
+  const _page = '<!--页面名称：'+ pageName +' 页面路径 '+ rootPath +'-->\r<template>\n  <div class="'+ pageName +'-page">'+ pageName +'</div>\n</template>\n<script type="text/javascript">\n  export default {\n    data() {\n      return{\n\n      }  \n    },\n    computed: {\n\n    },\n    beforeCreate() {\n\n    },\n    created() {\n\n    },\n    beforeMount() {\n\n    },\n    mounted() {\n\n    }, \n    beforeUpdate() {\n\n    }, \n    updated() {\n\n    }, \n    beforeDestroy() {\n\n    },\n    destroyed() {\n\n    }, \n    methods: {\n\n    },\n    watch: {\n\n    }\n  };\n</script>\n<style scoped> \n\n</style>'
 
 
   try {
@@ -25,11 +25,46 @@ router.post("/", async (req, res, next) => {
   } catch (error) {
     return Response.respondJSON(res, false, error.message)
   }
-  // 路由
-  fs.appendFile('./projects/'+ projectName +'/src/router.ts', 'data to append', (err) => {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-  });
+  // read
+  fs.readFile('./projects/'+ projectName +'/src/routerConfig.ts', 'utf-8', function(err, data){
+    if(err) {
+      console.log(err)
+    }
+    let fileData = data;
+
+    //  1、先插入import 引用
+    const _pageName = pageName.substring(0,1).toUpperCase() + pageName.substring(1);
+
+    const importPosition = fileData.indexOf('const routerConfig');
+    const importStr = 'import '+ _pageName +' from "./views/'+ pageName +'.vue";\n\n';
+    fileData = insetString(fileData, importStr, importPosition);
+    // 2、再插入路由配置
+    const routerPosition = fileData.indexOf('export default routerConfig;');
+    console.log(routerPosition)
+    const routerStr = '    {\n        path: "/'+ pageName +'",\n        component: '+ _pageName +'\n    },\n';
+    fileData = insetString(fileData, routerStr, routerPosition - 4);
+
+    // console.log(fileData)
+    fs.writeFile('./projects/'+ projectName +'/src/routerConfig.ts', fileData, (err) => {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+
+    //str 原字符串  flg 需要插入的字符串  pos 插入位置
+    function insetString(str, targetStr, pos){
+      const arrStr = new Array();
+      const leftStr = str.substring(0,pos);
+      const rightStr = str.substring(pos);
+      arrStr.push(leftStr);
+      arrStr.push(targetStr);
+      arrStr.push(rightStr);
+      const newStr = arrStr.join("");
+      return newStr
+    }
+  })
+
+
+
   return Response.respondJSON(
       res,
       true,
